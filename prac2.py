5bb7306ad6e267ea8221b9701a651458e217a6e7
woootaehyeon <padord@naver.com>
2023-09-25 21:17:48 +0900

    first commit

--- prac2.py
+++ prac2.py
@@ -1,16 +1,11 @@
 import sys
-input = sys.stdin.readline
+from collections import deque
+d = deque()
 n, m = map(int,input().split())
-d = []
-for i in range(n):
-    d.append(list(map(int,input().split())))
+for i in range(1,n+1):
+    d.append(i)
+print(d)
+check = deque(map(int,input().split()))
+cnt = 0
 
-k = int(input())
-for j in range(k):
-    sum = 0
-    i, j, x, y = map(int,input().split())
-    for o in range(i-1,x):
-        for p in range(j-1,y):
-            sum = sum + d[o][p]
-    print(sum)    
-
+print(d.index(5))
