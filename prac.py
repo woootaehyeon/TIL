26c7566f3c10c0f4554eca47fd9069d134afa819
woootaehyeon <padord@naver.com>
2024-01-16 14:40:15 +0900

    commit

--- prac.py
+++ prac.py
@@ -8,7 +8,6 @@ while True:
         for i in range(1,a):
             if a % i == 0:
                 arr.append(i)
-                
         if sum(arr) == a:
             print(f"{a} = ", end='')
             for i in range(len(arr) - 1):
