0e687982f68992e01ca356e0d1850476760c1e8c
woootaehyeon <padord@naver.com>
2024-01-16 14:28:39 +0900

    commit

--- prac.py
+++ prac.py
@@ -1,10 +1,18 @@
 import sys
-a, b = map(int,sys.stdin.readline().rsplit())
-arr = []
-for i in range(1,a+1):
-    if a % i == 0:
-        arr.append(i)
-if len(arr) < b:
-    print(0)
-else:
-    print(arr[b-1])
+while True:
+    a = int(sys.stdin.readline().rstrip())
+    if a == -1:
+        break
+    else:
+        arr = []
+        for i in range(1,a):
+            if a % i == 0:
+                arr.append(i)
+                
+        if sum(arr) == a:
+            print(f"{a} = ", end='')
+            for i in range(len(arr) - 1):
+                print(f"{arr[i]} + ", end='')
+            print(arr[-1])
+        else:
+            print(f"{a} is NOT perfect.")
