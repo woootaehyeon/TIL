6fbe9af75641d8885ea0481592a94e807af6ba1c
woootaehyeon <145675636+woootaehyeon@users.noreply.github.com>
2024-05-23 17:56:30 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -0,0 +1,10 @@
+n = input()
+length = len(n) - 1
+result = 0
+
+for i in range(length):
+    result += 9 * (10**i) * (i+1)
+    
+result += ((int(n) - (10 ** length))+1) * (length+1)
+
+print(result)
