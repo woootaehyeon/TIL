caa4e43ee524a523f981c1c2fff744af1419b111
woootaehyeon <145675636+woootaehyeon@users.noreply.github.com>
2024-06-17 17:49:25 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,15 +1,4 @@
 function solution(a, d, included) {
-    let arr = [];
-    let num = a;
-    let answer = 0;
-    for(let i = 0; i < included.length; i++){
-        arr.push(num);
-        num += d;
-    }
-    for(let j = 0; j < included.length; j++){
-        if(included[j] == true){
-            answer = answer + arr[j];
-        }
-    }
+    var answer = 0;
     return answer;
 }
